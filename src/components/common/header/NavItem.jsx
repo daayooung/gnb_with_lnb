a69437670b0b0ed873029da40641fee39e39e654
Kyungyoon Ha <gkb10a@naver.com>
2020-08-29 15:40:02 +0900

    사이드바 기능 수정

--- src/components/common/header/NavItem.jsx
+++ src/components/common/header/NavItem.jsx
@@ -1,28 +1,29 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
-import NavSub from './NavSub';
-import './NavItem.css';
+import React from "react";
+import { NavLink } from "react-router-dom";
+import NavSub from "./NavSub";
+import "./NavItem.css";
 
-const NavItem = ({ navInfo }) => {
-  const depth1Path = navInfo.path;
-  const depth1Title = navInfo.pagename;
-  const depth2 = navInfo.depth2;
+const NavItem = ({ navInfo, handleClickMenu }) => {
+    const depth1Path = navInfo.path;
+    const depth1Title = navInfo.pagename;
+    const depth2 = navInfo.depth2;
 
-  return (
-    <li className="depth1">
-      <NavLink
-        className="depth1_link"
-        activeClassName="active"
-        to={depth1Path}
-        exact
-      >
-        <span>{depth1Title}</span>
-      </NavLink>
-      {depth2.length > 0 && (
-        <NavSub depth1Path={navInfo.path} navInfo={depth2} />
-      )}
-    </li>
-  );
+    return (
+        <li className="depth1">
+            <NavLink
+                className="depth1_link"
+                activeClassName="active"
+                to={depth1Path}
+                exact
+                onClick={() => handleClickMenu(depth1Path)}
+            >
+                <span>{depth1Title}</span>
+            </NavLink>
+            {depth2.length > 0 && (
+                <NavSub depth1Path={navInfo.path} navInfo={depth2} />
+            )}
+        </li>
+    );
 };
 
 export default NavItem;
